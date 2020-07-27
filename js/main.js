fc97656b0afc01d7d477500e8b7405de3a3c04af
Wes Cook <im@wescook.ca>
2020-07-27 04:11:10 -0700

    Feedback on copy

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import {redditAccount, importSubreddit, exportSubreddits, trophies} from '../con
 
 let elemOutputOld = document.getElementById("output-old");
 let elemOutputNew = document.getElementById("output-new");
+let elemCopyOld = document.getElementById("output-old-copy");
+let elemCopyNew = document.getElementById("output-new-copy");
 
 // Load and embed trophies
 let elemTrophyList = document.getElementById("trophy-list");
@@ -50,3 +52,21 @@ elemOutputOld.addEventListener("focus", () => {
 elemOutputNew.addEventListener("focus", () => {
 	elemOutputNew.select();
 });
+
+
+elemCopyOld.addEventListener("click", () => {
+	copy(elemOutputOld.value);
+	elemCopyOld.textContent = "Copied!";
+});
+elemCopyNew.addEventListener("click", () => {
+	copy(elemOutputNew.value);
+	elemCopyNew.textContent = "Copied!";
+});
+
+
+function copy(text) {
+	navigator.clipboard.writeText(text)
+	.catch(err => {
+		console.error('Could not copy text: ', err);
+	});
+}
