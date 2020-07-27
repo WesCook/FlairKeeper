36cf718c89ff84652ce14676e6905299156acb0f
Wes Cook <im@wescook.ca>
2020-07-27 02:28:18 -0700

    Detecting clicks on trophies

--- config.sample.js
+++ config.sample.js
@@ -17,15 +17,16 @@ const exportSubreddits = [
 	"eldenringdiscussion"
 ]
 
+// List of game names, an icon to represent it, and the flair code it will produce
 const trophies = [
-	{name: "Demon's Souls", icon: "./img/trophies/platinum.png"},
-	{name: "Dark Souls", icon: "./img/trophies/bronze.png"},
-	{name: "Dark Souls 2", icon: "./img/trophies/gold.png"},
-	{name: "Bloodborne", icon: "./img/trophies/green.png"},
-	{name: "Dark Souls 3", icon: "./img/trophies/purple.png"},
-	{name: "Sekiro", icon: "./img/trophies/magenta.png"}
-	// {name: "Elden Ring", icon: "./img/trophies/charcoal.png"}
-	// {name: "ZZT", icon: "./img/trophies/ash.png"}
+	{id: "demonssouls", name: "Demon's Souls", icon: "./img/trophies/platinum.png", code: "De"},
+	{id: "darksouls1", name: "Dark Souls", icon: "./img/trophies/bronze.png", code: "Da"},
+	{id: "darksouls2", name: "Dark Souls II", icon: "./img/trophies/gold.png", code: "Da2"},
+	{id: "bloodborne", name: "Bloodborne", icon: "./img/trophies/green.png", code: "Bb"},
+	{id: "darksouls3", name: "Dark Souls III", icon: "./img/trophies/purple.png", code: "Da3"},
+	{id: "sekiro", name: "Sekiro", icon: "./img/trophies/magenta.png", code: "Sek"}
+	// {id: "eldenring", name: "Elden Ring", icon: "./img/trophies/charcoal.png", code: "Eld"}
+	// {id: "zzt", name: "ZZT", icon: "./img/trophies/ash.png", code: "ZZT"}
 ]
 
 export {redditAccount, importSubreddit, exportSubreddits, trophies};
--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 import {redditAccount, importSubreddit, exportSubreddits, trophies} from '../config.js';
 
 // Load and embed trophies
-let trophyList = document.getElementById("trophy-list");
+let elemTrophyList = document.getElementById("trophy-list");
 trophies.forEach(trophy => {
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let figcaption = document.createElement("figcaption");
 
+    figure.id = trophy.id;
     img.src = trophy.icon;
     img.alt = trophy.name;
     figcaption.textContent = trophy.name;
 
     figure.appendChild(img);
     figure.appendChild(figcaption);
-    trophyList.appendChild(figure);
+    figure.addEventListener("click", toggleTrophy);
+    elemTrophyList.appendChild(figure);
 });
+
+// Toggle trophies
+function toggleTrophy() {
+    console.log(this.id);
+}
