769eac8bd0b10a9a46621dacf73b442105c9fe94
Wes Cook <im@wescook.ca>
2020-07-29 05:49:53 -0700

    Checking auth response

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
-import {getAuth} from './modules/snoowrap.js';
+//import {getAuth} from './modules/snoowrap.js';
+import {redditApp} from '../config.js';
 
 //const reddit = getAuth();
 
 // Check cookie for connected OAuth account, set UI accordingly
-if (false) {
+// TODO: All of this, obviously
+if (true) {
 	document.body.classList.add("connected");
 }
+
+
+const reddit = snoowrap.fromAuthCode({
+	code: new URL(window.location.href).searchParams.get('code'),
+	userAgent: redditApp.userAgent,
+	clientId: redditApp.clientId,
+	redirectUri: window.location.origin + window.location.pathname + "settings.html"
+});
