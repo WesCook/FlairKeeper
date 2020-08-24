cd40f9bc479d72dabde68a5f3c69be3b8fb85818
Wes Cook <im@wescook.ca>
2020-08-24 10:15:06 -0700

    Updating SL1 icons

--- config.js
+++ config.js
@@ -17,32 +17,32 @@ const variants = ["Main", "SL1"];
 const trophies = [
 	{
 		id: "demonssouls", name: "Demon's Souls", variants: {
-			"Main": {css_text: "DeS", css_class: "De", icon: "img/trophies/platinum.png"},
-			"SL1": {css_text: "DeS-", css_class: "De-", icon: "img/trophies/platinum_darksign.png"}
+			"Main": {css_text: "DeS", css_class: "De", icon: "img/trophies/main/platinum.png"},
+			"SL1": {css_text: "DeS-", css_class: "De-", icon: "img/trophies/sl1/platinum.png"}
 		}
 	},
 	{
 		id: "darksouls1", name: "Dark Souls", variants: {
-			"Main": {css_text: "DaS", css_class: "Da", icon: "img/trophies/bronze.png"},
-			"SL1": {css_text: "DaS-", css_class: "Da-", icon: "img/trophies/bronze_darksign.png"}
+			"Main": {css_text: "DaS", css_class: "Da", icon: "img/trophies/main/bronze.png"},
+			"SL1": {css_text: "DaS-", css_class: "Da-", icon: "img/trophies/sl1/bronze.png"}
 		}
 	},
 	{
 		id: "darksouls2", name: "Dark Souls II", variants: {
-			"Main": {css_text: "DaS2", css_class: "Da2", icon: "img/trophies/gold.png"},
-			"SL1": {css_text: "DaS2-", css_class: "Da2-", icon: "img/trophies/gold_darksign.png"}
+			"Main": {css_text: "DaS2", css_class: "Da2", icon: "img/trophies/main/gold.png"},
+			"SL1": {css_text: "DaS2-", css_class: "Da2-", icon: "img/trophies/sl1/gold.png"}
 		}
 	},
 	{
 		id: "bloodborne", name: "Bloodborne", variants: {
-			"Main": {css_text: "Bb", css_class: "Bb", icon: "img/trophies/green.png"},
-			"SL1": {css_text: "Bb-", css_class: "Bb-", icon: "img/trophies/green_darksign.png"}
+			"Main": {css_text: "Bb", css_class: "Bb", icon: "img/trophies/main/green.png"},
+			"SL1": {css_text: "Bb-", css_class: "Bb-", icon: "img/trophies/sl1/green.png"}
 		}
 	},
 	{
 		id: "darksouls3", name: "Dark Souls III", variants: {
-			"Main": {css_text: "DaS3", css_class: "Da3", icon: "img/trophies/purple.png"},
-			"SL1": {css_text: "DaS3-", css_class: "Da3-", icon: "img/trophies/purple_darksign.png"}
+			"Main": {css_text: "DaS3", css_class: "Da3", icon: "img/trophies/main/purple.png"},
+			"SL1": {css_text: "DaS3-", css_class: "Da3-", icon: "img/trophies/sl1/purple.png"}
 		}
 	},
 	// Having gaps in variants is not a good idea, and may cause bugs.
@@ -51,14 +51,14 @@ const trophies = [
 	// eg. Disabling SL1 on Sekiro, then re-enabling it on Elden Ring.
 	{
 		id: "sekiro", name: "Sekiro", variants: {
-			"Main": {css_text: "Sek", css_class: "Sek", icon: "img/trophies/magenta.png"}
-			// "SL1": {css_text: "Sek-", css_class: "Sek-", icon: "img/trophies/magenta_darksign.png"}
+			"Main": {css_text: "Sek", css_class: "Sek", icon: "img/trophies/main/magenta.png"},
+			// "SL1": {css_text: "Sek-", css_class: "Sek-", icon: "img/trophies/sl1/magenta.png"}
 		}
 	},
 	// {
 	// 	id: "eldenring", name: "Elden Ring", variants: {
-	// 		"Main": {css_text: "Eld", css_class: "Eld", icon: "img/trophies/charcoal.png"}
-	// 		// "SL1": {css_text: "Eld-", css_class: "Eld-", icon: "img/trophies/charcoal_darksign.png"}
+	// 		"Main": {css_text: "Eld", css_class: "Eld", icon: "img/trophies/main/charcoal.png"},
+	// 		// "SL1": {css_text: "Eld-", css_class: "Eld-", icon: "img/trophies/sl1/charcoal.png"}
 	// 	}
 	// },
 ]
